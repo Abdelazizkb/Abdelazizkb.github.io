5e8ddd9487ea034095168b562b83069b46b0ee49
Abdelazizkb <korbaabdo@gmail.com>
2021-07-27 01:47:09 +0100

    form blanc

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function sendMail(params){
-    var tempParams={
-        from_name:document.getElementById("firstname").value+" "+document.getElementById("lastname").value+" "+document.getElementById("email").value,
-        to_name:"korba abdelaziz",
-        message:document.getElementById("message").value
+function sendMail(params) {
+    var tempParams = {
+        from_name: document.getElementById("firstname").value + " " + document.getElementById("lastname").value + " " + document.getElementById("email").value,
+        to_name: "korba abdelaziz",
+        message: document.getElementById("message").value
     };
-    emailjs.send('service_95twc0g','template_vymu68f',tempParams).then(res=>{
+    emailjs.send('service_95twc0g', 'template_vymu68f', tempParams).then(res => {
         console.log(res.status)
     })
+    document.getElementById("firstname").value = "";
+    document.getElementById("lastname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = ""
 }
