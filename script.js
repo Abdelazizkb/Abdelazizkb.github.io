cee71c1ee4f0a99e8a9847f14cce17a2a2971f97
Abdelazizkb <korbaabdo@gmail.com>
2021-07-29 02:36:30 +0100

    mail template changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function sendMail(params) {
     var tempParams = {
-        from_name: document.getElementById("firstname").value + " " + document.getElementById("lastname").value + " " + document.getElementById("email").value,
-        to_name: "korba abdelaziz",
+        name: document.getElementById("firstname").value + " " + document.getElementById("lastname").value,
+        email:document.getElementById("email").value,
         message: document.getElementById("message").value
     };
     emailjs.send('service_95twc0g', 'template_vymu68f', tempParams).then(res => {
@@ -10,5 +10,5 @@ function sendMail(params) {
     document.getElementById("firstname").value = "";
     document.getElementById("lastname").value = "";
     document.getElementById("email").value = "";
-    document.getElementById("message").value = ""
+    document.getElementById("message").value = "";
 }
